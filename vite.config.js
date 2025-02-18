208542ccb1207943efaad01a3432250543d05d61
Mukesh-2006x <mukesh2006xorg@gmail.com>
2025-02-18 10:21:47 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],base:"/SSG-task1"
+  plugins: [react()],base:"/SSG-task1/"
 })
